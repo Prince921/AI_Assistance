c9eb7ba2b1183194eb53424a2c392f44c5c0af6a
Adi <104979317+adigoyani@users.noreply.github.com>
2022-10-02 19:00:03 +0530

    more features added

--- speak.py
+++ speak.py
@@ -11,3 +11,18 @@ from speak immport speak
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+def Google():
+    speak('What should I search in Google?')
+    x = input('Type here')
+    speak('searching for'+x+'on Google')
+    search_Term = x.lower()
+    speak('Searching')
+    speak('Here we go to Google')
+    wb.open('http://www.google.com/search?q='+search_Term)
+def CPU():
+    usage = str(psutil.cpu_percent())
+    speak('Cpu is at')
+    speak(usage)
+    battery = psutil.sensors_battery()
+    speak('Battery is at')
+    speak(battery.percent) 
